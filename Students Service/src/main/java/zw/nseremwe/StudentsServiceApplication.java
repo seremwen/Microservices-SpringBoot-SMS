ceaa7fd0ca820cfc3b20260784e439615979d3cf
seremwen <seremwe.81@gmail.com>
2024-03-01 11:33:40 +0000

    added configuration files

---
+++ Students Service/src/main/java/zw/nseremwe/StudentsServiceApplication.java
@@ -0,0 +1,13 @@
+package zw.nseremwe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsServiceApplication.class, args);
+    }
+
+}
